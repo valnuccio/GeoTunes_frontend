8fed73210de6ca73383cac82187d998392593fda
valnuccio <val.nuccio@gmail.com>
2021-02-10 14:03:30 -0500

    fixed backend routes' ' '

--- src/railsserver.js
+++ src/railsserver.js
@@ -2,14 +2,14 @@
 
 
 
-const playroutes = "https://git.heroku.com/geotunes-backend-api.git/api/v1/play_routes/"
+const playroutes = "https://geotunes-backend-api.herokuapp.com//api/v1/play_routes/"
 
-const users = "https://git.heroku.com/geotunes-backend-api.git/api/v1/users/"
+const users = "https://geotunes-backend-api.herokuapp.com//api/v1/users/"
 
 
-const login = "https://git.heroku.com/geotunes-backend-api.git/api/v1/login/"
-const getUser = "https://git.heroku.com/geotunes-backend-api.git/api/v1/profile/"
+const login = "https://geotunes-backend-api.herokuapp.com//api/v1/login/"
+const getUser = "https://geotunes-backend-api.herokuapp.com//api/v1/profile/"
 
-const favCreate = "https://git.heroku.com/geotunes-backend-api.git/api/v1/fav_routes/"
+const favCreate = "https://geotunes-backend-api.herokuapp.com//api/v1/fav_routes/"
 
 export { playroutes, users, login, getUser, favCreate }   
