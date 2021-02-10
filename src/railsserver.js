502e0572c4ad09b6ff266679db0126e28e4c0ea9
valnuccio <val.nuccio@gmail.com>
2021-02-10 14:15:01 -0500

    fixed url typo

--- src/railsserver.js
+++ src/railsserver.js
@@ -2,14 +2,14 @@
 
 
 
-const playroutes = "https://geotunes-backend-api.herokuapp.com//api/v1/play_routes/"
+const playroutes = "https://geotunes-backend-api.herokuapp.com/api/v1/play_routes/"
 
-const users = "https://geotunes-backend-api.herokuapp.com//api/v1/users/"
+const users = "https://geotunes-backend-api.herokuapp.com/api/v1/users/"
 
 
-const login = "https://geotunes-backend-api.herokuapp.com//api/v1/login/"
-const getUser = "https://geotunes-backend-api.herokuapp.com//api/v1/profile/"
+const login = "https://geotunes-backend-api.herokuapp.com/api/v1/login/"
+const getUser = "https://geotunes-backend-api.herokuapp.com/api/v1/profile/"
 
-const favCreate = "https://geotunes-backend-api.herokuapp.com//api/v1/fav_routes/"
+const favCreate = "https://geotunes-backend-api.herokuapp.com/api/v1/fav_routes/"
 
 export { playroutes, users, login, getUser, favCreate }   
