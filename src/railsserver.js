9824604c7ac85c02ed5cdc1d53c480868015d3e9
valnuccio <val.nuccio@gmail.com>
2021-02-10 13:46:50 -0500

    test

--- src/railsserver.js
+++ src/railsserver.js
@@ -1,14 +1,15 @@
 
 
 
-const playroutes = "http://localhost:3000/api/v1/play_routes/"
 
-const users = "http://localhost:3000/api/v1/users/"
+const playroutes = "https://git.heroku.com/geotunes-backend-api.git/api/v1/play_routes/"
+
+const users = "https://git.heroku.com/geotunes-backend-api.git/api/v1/users/"
 
 
-const login = "http://localhost:3000/api/v1/login/"
-const getUser = "http://localhost:3000/api/v1/profile/"
+const login = "https://git.heroku.com/geotunes-backend-api.git/api/v1/login/"
+const getUser = "https://git.heroku.com/geotunes-backend-api.git/api/v1/profile/"
 
-const favCreate = "http://localhost:3000/api/v1/fav_routes/"
+const favCreate = "https://git.heroku.com/geotunes-backend-api.git/api/v1/fav_routes/"
 
-export { playroutes, users, login, getUser, favCreate }
+export { playroutes, users, login, getUser, favCreate }   
