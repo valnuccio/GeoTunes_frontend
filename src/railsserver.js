d91b1bd69709a3ad1d4e2dd58ab3ed1edc5d8593
valnuccio <val.nuccio@gmail.com>
2021-02-14 14:35:06 -0500

    with heroku routes ;

--- src/railsserver.js
+++ src/railsserver.js
@@ -2,14 +2,18 @@
 
 
 
-const playroutes = "http://localhost:3000/api/v1/play_routes/"
+const playroutes = "https://geotunes-backend-api.herokuapp.com/api/v1/play_routes/"
 
-const users = "http://localhost:3000/api/v1/users/"
+const users = "https://geotunes-backend-api.herokuapp.com/api/v1/users/"
 
 
-const login = "http://localhost:3000/api/v1/login/"
-const getUser = "http://localhost:3000/api/v1/profile/"
+const login = "https://geotunes-backend-api.herokuapp.com/api/v1/login/"
+const getUser = "https://geotunes-backend-api.herokuapp.com/api/v1/profile/"
 
-const favCreate = "http://localhost:3000/api/v1/fav_routes/"
+const favCreate = "https://geotunes-backend-api.herokuapp.com/api/v1/fav_routes/"
 
 export { playroutes, users, login, getUser, favCreate }   
+
+
+
+
